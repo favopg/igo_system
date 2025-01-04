9e320a4b85e659c0199ff0688fee2c0604963d7a
favopg <favoriteigo0904@gmail.com>
2025-01-04 09:04:51 +0900

    issue#13対応
    テストコードからも実施できるようにサンプル作成

--- src/test/java/jp/example/HelloServletTest.java
+++ src/test/java/jp/example/HelloServletTest.java
@@ -6,29 +6,57 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.nio.file.Paths;
 
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.DefaultServlet;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 import org.json.JSONObject;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserContext;
+import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
 
 class HelloServletTest {
 
 	private static Server server;
+	private static final Logger logger = LoggerFactory.getLogger(HelloServletTest.class);
+	
+	 private static Playwright playwright;
+	 private static Browser browser;
+	 private BrowserContext context;
+	 private Page page;
+
 
 	@BeforeAll
 	static void startServer() throws Exception {
 		server = new Server(8082);
 		ServletContextHandler context = new ServletContextHandler(ServletContextHandler.NO_SESSIONS);
-		context.setContextPath("/");
+		// 静的HTMLファイルのディレクトリを設定
+		context.setResourceBase(Paths.get("src/main/webapp").toAbsolutePath().toString());
+		//context.setContextPath("/");
 		server.setHandler(context);
 
 		// Servletの登録
-		context.addServlet(new ServletHolder(new HelloServlet()),"/hello");
+		context.addServlet(new ServletHolder(new GetJsonSampleServlet()),"/sample/igo_system");
+		context.addServlet(DefaultServlet.class,"/");
+
 		server.start();	
+		
+		// ここからplaywrightの設定
+		playwright = Playwright.create();
+		// ヘッドレスモードを無効化
+		browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false)); 
 	}
 
 	@AfterAll
@@ -36,13 +64,16 @@ class HelloServletTest {
 		if (server != null && server.isRunning()) {
 			server.stop();
 		}
+		
+		 //browser.close();
+	     playwright.close();
 	}
 
 	/**
 	 * jsonデータをServletに渡し、jsonデータを取得できること
 	 * @throws Exception
 	 */
-	@Test
+	@Disabled
 	void testHelloServlet() throws Exception {
 
 		// リクエストテストデータ
@@ -64,4 +95,35 @@ class HelloServletTest {
 		assertEquals(200, response.statusCode());
 		assertEquals("Hello, Jetty!", result.optString("message"));
 	}
+	
+	@ParameterizedTest
+	@CsvSource({
+	        "'黒', '白','黒勝ち','abc'",
+	        "'黒2', '白2','白勝ち','abc2'",
+	        "'黒3', '白3','白6目半勝ち','abc3'"
+	})
+	void testRegisterFromPlaywright(String blackPlayer, String whitePlayer, String result, String kifu) {
+		
+		System.out.println("テストデータ確認" + blackPlayer + whitePlayer + result + kifu);
+		
+		 // 新しいコンテキストとページを作成
+        context = browser.newContext();
+        page = context.newPage();
+
+        // テスト対象のURLに移動
+        page.navigate("http://localhost:8082/sample/register.html");
+
+        // フォームフィールドに値を入力
+        page.locator("#blackPlayer").fill(blackPlayer);
+        page.locator("#whitePlayer").fill(whitePlayer);
+        page.locator("#result").fill(result);
+        page.locator("#kifu").fill(kifu);
+
+        // ボタンをクリック
+        page.locator("#submit").click();
+
+        // コンテキストを閉じる
+        context.close();
+	}
+	
 }
