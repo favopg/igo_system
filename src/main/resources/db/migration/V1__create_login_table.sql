d037c5be91de57c86eec731d94d4051747df7114
favopg <favoriteigo0904@gmail.com>
2025-01-08 22:48:14 +0900

    issues#20対応

---
+++ src/main/resources/db/migration/V1__create_login_table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE login (
+    username VARCHAR(20) NOT NULL,       -- ユーザ名（全角20文字まで）
+    password_hash CHAR(65) NOT NULL,    -- パスワード（ハッシュ化済み、固定長65文字）
+    public_flag SMALLINT NOT NULL CHECK (public_flag IN (0, 1)), -- 公開/非公開フラグ
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,     -- レコード作成日時
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL      -- レコード更新日時
+);
