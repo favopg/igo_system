846b015e786603c46bb152f7cef9c4c710f07250
favopg <favoriteigo0904@gmail.com>
2025-01-13 08:56:05 +0900

    issues#40対応
    domaモジュールのコミット漏れ

---
+++ src/main/java/jp/example/AppConfig.java
@@ -0,0 +1,48 @@
+package jp.example;
+
+
+import org.seasar.doma.jdbc.*;
+import org.seasar.doma.jdbc.dialect.Dialect;
+import org.seasar.doma.jdbc.dialect.MysqlDialect;
+import org.seasar.doma.jdbc.tx.LocalTransactionDataSource;
+import org.seasar.doma.jdbc.tx.LocalTransactionManager;
+import org.seasar.doma.jdbc.tx.TransactionManager;
+
+import javax.sql.DataSource;
+
+public class AppConfig implements Config {
+
+    private final Dialect dialect;
+
+    private final LocalTransactionDataSource dataSource;
+
+    private final TransactionManager transactionManager;
+
+    public AppConfig() {
+        dialect = new MysqlDialect();
+        dataSource = new LocalTransactionDataSource(
+                PropertyUtil.getProperty("db.url"),
+                PropertyUtil.getProperty("db.user"),
+                PropertyUtil.getProperty("db.password")
+        );
+        transactionManager = new LocalTransactionManager(
+                dataSource.getLocalTransaction(getJdbcLogger()));
+    }
+
+
+    @Override
+    public Dialect getDialect() {
+        return dialect;
+    }
+
+    @Override
+    public DataSource getDataSource() {
+        return dataSource;
+    }
+
+    @Override
+    public TransactionManager getTransactionManager() {
+        return transactionManager;
+    }
+
+}
