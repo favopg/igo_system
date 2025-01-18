e5ba866f73c53a645fc66140781f807c21aca9c0
favopg <favoriteigo0904@gmail.com>
2025-01-18 13:40:10 +0900

    issues#44対応
    APIステータス系のコードをenumで持つように追加
    コミット忘れ

---
+++ src/main/java/jp/example/ApiResponse.java
@@ -0,0 +1,15 @@
+package jp.example;
+
+public enum ApiResponse {
+    OK("success"), NG("error"), STATUS("status");
+
+    private final String code;
+
+    ApiResponse(String code) {
+        this.code = code;
+    }
+
+    public String getCode() {
+        return code;
+    }
+}
