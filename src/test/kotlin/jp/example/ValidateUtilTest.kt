64fdc23c83001c0f669271d71444d337f77ab81f
favopg <favoriteigo0904@gmail.com>
2025-01-19 19:39:58 +0900

    issues#43、#49対応
    対応によるコンパイルエラー対応(コミット忘れ)

--- src/test/kotlin/jp/example/ValidateUtilTest.kt
+++ src/test/kotlin/jp/example/ValidateUtilTest.kt
@@ -2,12 +2,13 @@ package jp.example
 
 import org.json.JSONObject
 import org.junit.jupiter.api.Assertions.*
+import org.mockito.internal.matchers.Matches
 import kotlin.test.Test
 
 class ValidateUtilTest {
     @Test
     fun validateTest() {
-       val testData = FormData()
+       val testData = MatchForm()
        val result : JSONObject = ValidateUtil.validate(testData)
         assertEquals(result.optString("status"), "error")
     }
