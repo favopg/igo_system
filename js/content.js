3a3c134d4b970145536a6087ecc8cbe68b7b918e
favopg <favoriteigo0904@gmail.com>
2025-02-01 11:11:47 +0900

    モーダルを部品化

--- js/content.js
+++ js/content.js
@@ -7,8 +7,8 @@ const naviSidever = Vue.createApp({
 });
 
 naviSidever.component("navi_sideber", {
-  props: ['activekbn'],
-	template: `
+    props: ['activekbn'],
+    template: `
 		<div class="sidebar-content">
     		<h3 class="text-white mb-4">メニュー</h3>
               <ul class="nav flex-column mb-auto nav-pills">
@@ -35,6 +35,45 @@ naviSidever.component("navi_sideber", {
 	`,
 });
 naviSidever.mount('#sidebar')
+
+// ここからモーダル部品
+const modal = Vue.createApp({
+    data() {
+      return {
+        // モーダルのデフォルト情報
+        modalinfo: {
+            title:"削除",
+            detail:"選択した行を削除します。よろしいですか？",
+            dangerinfo:"いいえ",
+            primaryinfo:"はい"
+        }
+      };
+    },
+  });
+  
+  modal.component("modal", {
+      props: ['modalinfo'],
+      template: `
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="confirmModalLabel">{{modalinfo.title}}</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    {{modalinfo.detail}}
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">{{ modalinfo.dangerinfo }}</button>
+                    <button type="button" class="btn btn-primary">{{ modalinfo.primaryinfo}}</button>
+                </div>
+            </div>
+        </div>
+      `,
+  });
+  modal.mount('#confirmModal')
+  // ここまでモーダル
+  
 
 
 // ここからはコンテンツ部分
